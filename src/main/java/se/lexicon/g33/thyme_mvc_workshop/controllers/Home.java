e2993fe82e537dcf8443afe7199b33d6a4dd9145
AndreasMangs <andreas_mangs@hotmail.com>
2021-03-22 14:47:44 +0100

    added input and output

--- src/main/java/se/lexicon/g33/thyme_mvc_workshop/controllers/Home.java
+++ src/main/java/se/lexicon/g33/thyme_mvc_workshop/controllers/Home.java
@@ -1,18 +1,35 @@
 package se.lexicon.g33.thyme_mvc_workshop.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class Home {
 
+    private List<String> inputs = new ArrayList<>();
+
     @GetMapping("/index")
-    String index() {
+    public String index() {
         return "index";
     }
     @GetMapping("/contact")
-    String contact(){
+    public String contact(Model model){
+        model.addAttribute("inputs", inputs);
         return "contact";
     }
+
+    @PostMapping("/contact")
+    public String contact(@RequestParam("input") String input){
+        inputs.add(input);
+        return "redirect:/contact";
+    }
+
+
 
 }
